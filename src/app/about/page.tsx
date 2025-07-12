5bf5a60eb5e78ae78b46448c363d71729ce32405
Jhxst <jhastinkennethnarciso04@gmail.com>
2025-07-13 03:00:46 +0800

    First Commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function Page2() {
+    return(
+        <main className="bg-pink-100 min-h-screen flex items-center justify-center">
+            <h1>Page 2</h1>
+        </main>
+    )
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "~/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "IPT First Project",
+  description: "Integrated Project Team First Project",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <main className="flex w-full">
+        <h1>Hello IT-3B</h1>
+      </main>
+      <body>{children}</body>
+    </html>
+  );
+}
